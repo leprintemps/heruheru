a6682aa6f6b84990121ff67abcd8e13f5605a182
우영래 <heruheru@uyeonglaeui-MacBookAir.local>
2022-10-14 18:49:03 +0900

    두번째 커밋

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const express = require('express');
-const monggose = require('mongoose');
 
 const app = express();
 const port = 3000
 
-monggose.connect('mongodb+srv://heruheru:dkspahsp77@heruheru.4yzkrxc.mongodb.net/?retryWrites=true&w=majority', {
+const monggose = require('mongoose');
+const bodyParser = require('body-parser');
+const { User } = require("./models/User"); // User.js모델 가져오기
+
+const config = require('./config/key');
+
+// application/x-www-form-urlendcoded 데이터를 분석할 수 있도록
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json 데이터를 분석할 수 있도록 
+app.use(bodyParser.json());
+
+monggose.connect(config.mongoURI, {
     useNewUrlParser : true
 }).then(() => console.log('MongoDB Connected')).catch((err) => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+// 회원가입을 위한 라우터
+app.post('/resister', (req,res) => {
+    // 회원가입할때 필요한 정보들을 client에서 가져오면 그 값들을 데이터 베이스에 넣어준다.
+    const user = new User(req.body);
+
+    // 몽공db에 유저정보 저장
+    user.save((err, userInfo) => {
+        if(err){
+            return res.json({success: false, err});
+        } else {
+            return res.status(200).json({
+                success: true
+            })
+        }
+    });
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
